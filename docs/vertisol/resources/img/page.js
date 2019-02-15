adeb4919785d82bc76574f6af6ed39d4b49b0f7c
bythealphabet <bythealphabet@gmail.com>
2019-02-15 15:04:29 -0400

    finished making the photo gallery for the wood blinds page

--- docs/vertisol/resources/img/page.js
+++ docs/vertisol/resources/img/page.js
@@ -1,61 +1,61 @@
 const listItem1 = document.getElementById('list-item-1')
-// const listItem2 = document.getElementById('list-item-2')
-// const listItem3 = document.getElementById('list-item-3')
-// const listItem4 = document.getElementById('list-item-4')
-// const listItem5 = document.getElementById('list-item-5')
-// const listItem6 = document.getElementById('list-item-6')
-// const listItem7 = document.getElementById('list-item-7')
-// const listItem8 = document.getElementById('list-item-8')
-// const listItem9 = document.getElementById('list-item-9')
-// const listItem10 = document.getElementById('list-item-10')
-// const listItem11 = document.getElementById('list-item-11')
-// const listItem12 = document.getElementById('list-item-12')
+const listItem2 = document.getElementById('list-item-2')
+const listItem3 = document.getElementById('list-item-3')
+const listItem4 = document.getElementById('list-item-4')
+const listItem5 = document.getElementById('list-item-5')
+const listItem6 = document.getElementById('list-item-6')
+const listItem7 = document.getElementById('list-item-7')
+const listItem8 = document.getElementById('list-item-8')
+const listItem9 = document.getElementById('list-item-9')
+const listItem10 = document.getElementById('list-item-10')
+const listItem11 = document.getElementById('list-item-11')
+const listItem12 = document.getElementById('list-item-12')
 
 listItem1.addEventListener('mouseenter',()=>{
 	console.log('it works')
-  // showcase.src = "blinds_and_shades/wood_blinds/1.jpg"
+  showcase.src = listItem1.src
 })
 
-// listItem2.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/2.jpg"
-// })
+listItem2.addEventListener('mouseenter',()=>{
+  showcase.src = listItem2.src
+})
 
-// listItem3.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/3.jpg"
-// })
+listItem3.addEventListener('mouseenter',()=>{
+  showcase.src = listItem3.src
+})
 
-// listItem4.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/4.jpg"
-// })
+listItem4.addEventListener('mouseenter',()=>{
+  showcase.src = listItem4.src
+})
 
-// listItem5.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/5.jpg"
-// })
+listItem5.addEventListener('mouseenter',()=>{
+  showcase.src = listItem5.src
+})
 
-// listItem6.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/6.jpg"
-// })
+listItem6.addEventListener('mouseenter',()=>{
+  showcase.src = listItem6.src
+})
 
-// listItem7.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/7.jpg"
-// })
+listItem7.addEventListener('mouseenter',()=>{
+  showcase.src = listItem7.src
+})
 
-// listItem8.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/8.jpg"
-// })
+listItem8.addEventListener('mouseenter',()=>{
+  showcase.src = listItem8.src
+})
 
-// listItem9.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/9.jpg"
-// })
+listItem9.addEventListener('mouseenter',()=>{
+  showcase.src = listItem9.src
+})
 
-// listItem10.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/10.jpg"
-// })
+listItem10.addEventListener('mouseenter',()=>{
+  showcase.src = listItem10.src
+})
 
-// listItem11.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/11.jpg"
-// })
+listItem11.addEventListener('mouseenter',()=>{
+  showcase.src = listItem11.src
+})
 
-// listItem12.addEventListener('mouseenter',()=>{
-//   showcase.src = "resources/img/12.jpg"
-// })
+listItem12.addEventListener('mouseenter',()=>{
+  showcase.src = listItem12.src
+})
