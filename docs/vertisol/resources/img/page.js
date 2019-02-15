14eec39471016f2e7d6e25e9ccfb3648d9949f61
bythealphabet <bythealphabet@gmail.com>
2019-02-15 13:57:14 -0400

    working on wood blinds page added the js file to the page

---
+++ docs/vertisol/resources/img/page.js
@@ -0,0 +1,61 @@
+const listItem1 = document.getElementById('list-item-1')
+// const listItem2 = document.getElementById('list-item-2')
+// const listItem3 = document.getElementById('list-item-3')
+// const listItem4 = document.getElementById('list-item-4')
+// const listItem5 = document.getElementById('list-item-5')
+// const listItem6 = document.getElementById('list-item-6')
+// const listItem7 = document.getElementById('list-item-7')
+// const listItem8 = document.getElementById('list-item-8')
+// const listItem9 = document.getElementById('list-item-9')
+// const listItem10 = document.getElementById('list-item-10')
+// const listItem11 = document.getElementById('list-item-11')
+// const listItem12 = document.getElementById('list-item-12')
+
+listItem1.addEventListener('mouseenter',()=>{
+	console.log('it works')
+  // showcase.src = "blinds_and_shades/wood_blinds/1.jpg"
+})
+
+// listItem2.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/2.jpg"
+// })
+
+// listItem3.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/3.jpg"
+// })
+
+// listItem4.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/4.jpg"
+// })
+
+// listItem5.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/5.jpg"
+// })
+
+// listItem6.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/6.jpg"
+// })
+
+// listItem7.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/7.jpg"
+// })
+
+// listItem8.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/8.jpg"
+// })
+
+// listItem9.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/9.jpg"
+// })
+
+// listItem10.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/10.jpg"
+// })
+
+// listItem11.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/11.jpg"
+// })
+
+// listItem12.addEventListener('mouseenter',()=>{
+//   showcase.src = "resources/img/12.jpg"
+// })
